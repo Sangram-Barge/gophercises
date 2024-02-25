aab00f09dec1635dd20d33f0d8c1fba0f9458b15
Sangram Barge <sangrambarge098@gmail.com>
2024-02-25 21:11:01 +0530

    quiz non timed

---
+++ quiz/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
+
+type problem struct {
+	question string
+	answer   string
+}
+
+func main() {
+	csvFileName := flag.String("csv", "problems.csv", "csv file location")
+	flag.Parse()
+
+	file, err := os.Open(*csvFileName)
+	if err != nil {
+		exit(fmt.Sprintf("cannot open file %s \n", *csvFileName))
+	}
+
+	lines, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		exit(fmt.Sprintf("error parsing file %s \n", *csvFileName))
+	}
+
+	problems := parseProblems(lines)
+
+	correctCount := 0
+	for i, prob := range problems {
+		fmt.Printf("Problem #%d: %s\n", i+1, prob.question)
+		var ans string
+		fmt.Scanf("%s\n", &ans)
+		if ans == prob.answer {
+			correctCount++
+		}
+	}
+	fmt.Printf("You scared %d out of %d\n", correctCount, len(problems))
+}
+
+func parseProblems(lines [][]string) []problem {
+	problems := make([]problem, len(lines))
+	for i, line := range lines {
+		problems[i] = problem{
+			question: line[0],
+			answer:   line[1],
+		}
+	}
+	return problems
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
